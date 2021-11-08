e47449610a4b029bb86b037e353601c75a8758a4
Emanuelguantay <emanuel.guantay92@gmail.com>
2021-11-08 00:29:29 -0300

    .

--- database/config.js
+++ database/config.js
@@ -1,11 +1,21 @@
+const { Console } = require('console');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const dbConnection = async() =>{
     try{
-        console.log('INIT DB CONFIG')
+        
+            await mongoose.connect("mongodb+srv://root:g2unCxkiXgAGZD18@cluster0.sohoq.mongodb.net/chat", {
+            useNewUrlParser: true,
+            //useUnifiedTopology: true,
+            //useCreateIndex: true
+                
+        })
+
+        console.log('DB ONLINE')
     }catch(e){
         console.log(e);
-        throw new Error('Error en la base de datos');
+        //throw new Error('Error en la base de datos');
     }
 
 }
