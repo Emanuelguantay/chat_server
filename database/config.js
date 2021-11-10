f99b73ae589dea502b8d771e3b33f156de61bb01
Emanuelguantay <emanuel.guantay92@gmail.com>
2021-11-10 19:39:54 -0300

    Fix .env

--- database/config.js
+++ database/config.js
@@ -2,14 +2,16 @@ const { Console } = require('console');
 const mongoose = require('mongoose');
 const path = require('path');
 
+require('dotenv').config()
+
 const dbConnection = async() =>{
     try{
-        
-            await mongoose.connect("mongodb+srv://root:g2unCxkiXgAGZD18@cluster0.sohoq.mongodb.net/chat", {
+        const url = process.env.DB_CNN
+        await mongoose.connect(url, {
             useNewUrlParser: true,
-            //useUnifiedTopology: true,
+            useUnifiedTopology: true,
             //useCreateIndex: true
-                
+            //Aprtir de Mongoose 6.0 no es necesario es true por default  
         })
 
         console.log('DB ONLINE')
