48874dc7f28682326c041c6724f100b4f9feff22
Emanuelguantay <emanuel.guantay92@gmail.com>
2021-11-11 00:14:50 -0300

    check endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const createUser = (req, resp = response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()){
+        return resp.status(400).json({
+            ok: false,
+            msg: errors.mapped()
+        });
+    }
     resp.json({
         ok: true,
         msg: 'Crear usuario!!!'
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,15 @@
  * path: api/login
  */
 const { Router, response} = require('express');
+const { check } = require('express-validator');
+
 const { createUser } = require('../controllers/auth');
 
 const router = Router();
 
-router.post('/new', createUser);
+router.post('/new',[
+    check('name', 'Nombre es obligatorio').not().isEmpty(),
+    
+] ,createUser);
 
 module.exports = router;
