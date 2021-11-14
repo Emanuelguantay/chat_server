b9d3e02823392408b6afab9dbe2638813931c314
Emanuelguantay <emanuel.guantay92@gmail.com>
2021-11-13 22:37:06 -0300

    Validators New Customer
    Validaciones agregada para el endpoint de nuevo usuario

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,15 @@ const { Router, response} = require('express');
 const { check } = require('express-validator');
 
 const { createUser } = require('../controllers/auth');
+const { validateField } = require('../middlewares/validate-field');
 
 const router = Router();
 
 router.post('/new',[
     check('name', 'Nombre es obligatorio').not().isEmpty(),
+    check('email', 'Email es obligatorio').not().isEmail(),
+    check('password', 'Contraseña es obligatoria').isEmpty(),
+    validateField,
     
 ] ,createUser);
 
